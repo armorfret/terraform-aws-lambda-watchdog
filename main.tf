57ea5154029c19ac412144b1d72aa68949a7619b
Les Aker <me@lesaker.org>
2022-10-02 16:50:02 -0400

    update module ref

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.2.2"
+  version = "0.2.3"
 
   source_bucket  = var.lambda_bucket
   source_version = var.lambda_version
