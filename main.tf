0412a44a47e4c29601ffd0a0f716dc65681c648c
Les Aker <me@lesaker.org>
2024-04-03 15:33:31 -0400

    update lambda logging

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 module "apigw" {
   source  = "armorfret/apigw-lambda/aws"
-  version = "0.9.0"
+  version = "0.9.1"
 
   source_bucket  = var.lambda_bucket
   source_version = var.lambda_version
@@ -94,7 +94,6 @@ data "aws_iam_policy_document" "lambda_perms" {
 
   statement {
     actions = [
-      "logs:CreateLogGroup",
       "logs:CreateLogStream",
     ]
 
